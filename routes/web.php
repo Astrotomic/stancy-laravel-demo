4dc8620cf0727a6b125c70d21528bf145a98fc14
Gummibeer <dev.gummibeer@gmail.com>
2019-10-08 14:48:55 +0200

    use facade instead of container

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
-use Astrotomic\Stancy\Contracts\PageFactory;
+use Astrotomic\Stancy\Facades\PageFactory;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return app(PageFactory::class)->makeFromSheetName('static', 'home');
+    return PageFactory::makeFromSheetName('static', 'home');
 });
 
 Route::get('/blog', function () {
-    return app(PageFactory::class)->makeFromSheetName('static', 'blog');
+    return PageFactory::makeFromSheetName('static', 'blog');
 });
 
 Route::get('/blog/{post}', function (string $post) {
-    return app(PageFactory::class)->makeFromSheetName('blog', $post);
+    return PageFactory::makeFromSheetName('blog', $post);
 })->name('blog.post');
 
 Route::feeds();
